c1fb69f68c5a5c1860b1c7d47d68f2b2799f0527
Charles Luedecke <cluedecke@gmail.com>
2025-08-06 04:12:57 -0400

    fixed file path for frankenstein.txt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def get_book_text(path):
         return f.read()
 
 def main():
-    book = get_book_text("frankenstein.txt")
+    book = get_book_text("books/frankenstein.txt")
     print(f"{total_words(book)} words found in the document")
     char_counts = char_count(book)
     for char, counts in char_counts.items():
